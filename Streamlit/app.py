2b6c5c4799ca62e00a52267fccefd57e05caeaf2
Sarah Valme <svalm97@gmail.com>
2024-11-13 05:11:55 -0800

    adding streamlit directory and working simple streamlit app

---
+++ Streamlit/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+#import requests
+
+
+
+def main():
+    st.title('Streamlit Front-End on Google Cloud Run')
+    
+    
+    # Display the data
+    st.write(f"Hello, working")
+
+if __name__ == '__main__':
+    main()
